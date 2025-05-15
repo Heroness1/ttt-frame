132b7137f6424d9f720c7275c0d27e942db3648c
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-15 22:24:31 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-'use client';
+// app/page.tsx
+import NadShoott from "../components/NadShoott";
 
-import React from 'react';
-import TicTacToe from '@/components/TicTacToe';
-
-export default function App() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Tic Tac Toe - Pixel Style</h1>
-      <TicTacToe />
-    </main>
-  );
+export default function HomePage() {
+  return <NadShoott />;
 }
