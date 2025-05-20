ebd4f3090b4ea8ec8a5c7630cfc5e9934a447b86
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-20 11:18:45 +0700

    Update constans.js

--- components/constans.js
+++ components/constans.js
@@ -1,130 +1,28 @@
 
+
 export const ROWS = 14;
 export const COLS = 10;
-export const INTERVAL = 800;
 
-export const TETROMINOS = {
-  I: {
-    shape: [
-      [
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-      ],
-      [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-      ],
-    ],
-    color: "cyan",
-  },
-  O: { shape: [[[1, 1], [1, 1]]], color: "yellow" },
-  T: {
-    shape: [
-      [
-        [0, 1, 0],
-        [1, 1, 1],
-        [0, 0, 0],
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 1, 0],
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 1, 0],
-      ],
-      [
-        [0, 1, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-      ],
-    ],
-    color: "purple",
-  },
-  S: {
-    shape: [
-      [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1],
-      ],
-    ],
-    color: "green",
-  },
-  Z: {
-    shape: [
-      [
-        [1, 1, 0],
-        [0, 1, 1],
-        [0, 0, 0],
-      ],
-      [
-        [0, 0, 1],
-        [0, 1, 1],
-        [0, 1, 0],
-      ],
-    ],
-    color: "red",
-  },
-  J: {
-    shape: [
-      [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0],
-      ],
-      [
-        [0, 1, 1],
-        [0, 1, 0],
-        [0, 1, 0],
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [0, 0, 1],
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 0],
-      ],
-    ],
-    color: "blue",
-  },
-  L: {
-    shape: [
-      [
-        [0, 0, 1],
-        [1, 1, 1],
-        [0, 0, 0],
-      ],
-      [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 1],
-      ],
-      [
-        [0, 0, 0],
-        [1, 1, 1],
-        [1, 0, 0],
-      ],
-      [
-        [1, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-      ],
-    ],
-    color: "orange",
-  },
+export const INTERVAL = 500;
+
+export const TETROMINO_COLORS = {
+  I: "cyan",
+  O: "yellow",
+  T: "purple",
+  S: "green",
+  Z: "red",
+  J: "blue",
+  L: "orange",
 };
+
+export const DIRECTIONS = {
+  LEFT: { x: -1, y: 0 },
+  RIGHT: { x: 1, y: 0 },
+  DOWN: { x: 0, y: 1 },
+};
+
+export const CELL_SIZE = 30;
+
+export const BASE_SCORE = 100;
+export const COMBO_MULTIPLIER = 1.5;
+export const EXPLOSION_THRESHOLD = 3;
