dda5103d9929b0683884999361d13f8b8251f69d
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-22 20:04:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-// app/page.tsx
-import NadShoott from "../components/NadShoott";
+import Head from 'next/head'
+import NadShoott from '../components/NadShoott'
 
-export default function HomePage() {
-  return <NadShoott />;
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta
+          name="fc:frame"
+          content='{
+            "version":"vNext",
+            "imageUrl":"https://ttt-frame.vercel.app/images/splash.png",
+            "button":{"title":"Play"},
+            "postUrl":"https://ttt-frame.vercel.app/api/frame"
+          }'
+        />
+        <title>Tetra</title>
+      </Head>
+      <NadShoott />
+    </>
+  )
 }
