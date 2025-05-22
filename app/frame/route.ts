815fd37d35104e8d383d22398a8a051e86d89c74
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-22 14:20:18 +0700

    Update route.ts

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -2,11 +2,11 @@ export async function GET() {
   return new Response(
     JSON.stringify({
       "@context": "https://w3id.org/farcaster/frames/v1.0.0",
-      "frame:image": "https://ttt-frame.vercel.app/api/og", // image yang ditampilkan di frame
+      "frame:image": "https://ttt-frame.vercel.app/api/og", 
       "frame:buttons": [
         { label: "Connect Wallet", action: "post" }
       ],
-      "frame:post_url": "https://ttt-frame.vercel.app/api/frame", // ini bakal nerima wallet-nya
+      "frame:post_url": "https://ttt-frame.vercel.app/api/frame",
       "frame:requires": "wallet"
     }),
     {
