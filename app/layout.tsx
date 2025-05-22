c66c33ec67be0d30e1182adb711f816d9f7fe5aa
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-23 00:36:07 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,24 @@
-// app/layout.tsx
+import "./globals.css";
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["500"],
+});
+
 export const metadata = {
-  title: "TetraMON",
-  description: "Score with some explode!.",
-  openGraph: {
-    title: "TetraMON",
-    description: "Break Monad v2",
-    images: [
-      {
-        url: "https://ttt-frame.vercel.app/og-image.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  other: {
-    "fc:frame": "vNext",
-    "fc:frame:image": "https://yourdomain.vercel.app/og-image.png",
-    "fc:frame:button:1": "Play Now",
-    "fc:frame:button:1:action": "post_redirect",
-    "fc:frame:post_url": "https://ttt-frame.vercel.app/game",
-  },
+  title: "nadshoott",
+  description: "shhhoot!!",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
-      </head>
-      <body style={{ fontFamily: '"Press Start 2P", monospace' }}>
-        {children}
-      </body>
+      <body className={orbitron.className}>{children}</body>
     </html>
   );
 }
