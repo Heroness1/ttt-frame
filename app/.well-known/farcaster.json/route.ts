42d74849984912403419c042097de9dc5e454388
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-05-22 19:10:24 +0700

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,16 +1,22 @@
-{
-  "frame": {
-    "version": "1",
-    "name": "Tetra MON",
-    "iconUrl": "https://ttt-frame.vercel.app/images/icon.png",
-    "homeUrl": "https://ttt-frame.vercel.app",
-    "imageUrl": "https://ttt-frame.vercel.app/images/feed.png",
-    "screenshotUrls": [],
-    "tags": ["monad", "game", "farcaster"],
-    "primaryCategory": "developer-tools",
-    "buttonTitle": "Play Now",
-    "splashImageUrl": "https://ttt-frame.vercel.app/images/splash.png",
-    "splashBackgroundColor": "#ffffff",
-    "webhookUrl": "https://ttt-frame.vercel.app/api/webhook"
-  }
+import { type NextRequest } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export function GET(_req: NextRequest) {
+  return Response.json({
+    frame: {
+      version: "1",
+      name: "Tetra MON",
+      iconUrl: "https://ttt-frame.vercel.app/images/icon.png",
+      homeUrl: "https://ttt-frame.vercel.app",
+      imageUrl: "https://ttt-frame.vercel.app/images/feed.png",
+      screenshotUrls: [],
+      tags: ["monad", "farcaster", "miniapp", "game"],
+      primaryCategory: "developer-tools",
+      buttonTitle: "Play",
+      splashImageUrl: "https://ttt-frame.vercel.app/images/splash.png",
+      splashBackgroundColor: "#ffffff",
+      webhookUrl: "https://ttt-frame.vercel.app/api/webhook",
+    },
+  });
 }
