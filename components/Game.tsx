d7ee7fcdd8576537973a1476e74a3b78b23e4701
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-10-22 01:22:30 +0700

    Update Game.tsx

--- components/Game.tsx
+++ components/Game.tsx
@@ -1,10 +1,15 @@
+"use client";
 import React from "react";
-import TetrisBoard from "../components/TetrisBoard";
+import TetrisBoard from "./TetrisBoard";
+import { useSearchParams } from "next/navigation";
 
-export default function GamePage() {
+export default function Game() {
+  const searchParams = useSearchParams();
+  const wallet = searchParams.get("wallet"); // ambil wallet dari URL (misal /game?wallet=0x123)
+
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      <TetrisBoard />
+      <TetrisBoard wallet={wallet} />
     </div>
   );
 }
