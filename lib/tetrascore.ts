b6dbdb5e9920a270879ed75e62fa926609623046
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-10-21 12:03:29 +0700

    Add TETRA_SCORE_ADDRESS and ABI definitions

---
+++ lib/tetrascore.ts
@@ -0,0 +1,44 @@
+export const TETRA_SCORE_ADDRESS = "0xb6F7A3e43F2B22e5f73162c29a12c280A8c20db2";
+
+export const TETRA_SCORE_ABI = [
+  {
+    "inputs": [
+      { "internalType": "uint256", "name": "score", "type": "uint256" }
+    ],
+    "name": "saveScore",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      { "indexed": true, "internalType": "address", "name": "player", "type": "address" },
+      { "indexed": false, "internalType": "uint256", "name": "newScore", "type": "uint256" }
+    ],
+    "name": "ScoreUpdated",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "player", "type": "address" }
+    ],
+    "name": "getScore",
+    "outputs": [
+      { "internalType": "uint256", "name": "", "type": "uint256" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "name": "highScores",
+    "outputs": [
+      { "internalType": "uint256", "name": "", "type": "uint256" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
