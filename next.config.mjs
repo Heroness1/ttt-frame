ec57136cad312b63cc0756d0423c82aef4ac9cff
Heroness1 <161195189+Heroness1@users.noreply.github.com>
2025-10-24 21:37:09 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "@react-native-async-storage/async-storage": false,
-    };
-    return config;
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@metamask/delegation-toolkit",
+      "permissionless",
+    ],
   },
 };
 
